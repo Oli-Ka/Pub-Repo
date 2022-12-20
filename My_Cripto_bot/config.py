8895074db7d841d37975d33501b722934addc8a7
Oli-Ka <114650712+Oli-Ka@users.noreply.github.com>
2022-12-21 00:18:29 +0400

    Add files via upload

---
+++ My_Cripto_bot/config.py
@@ -0,0 +1,10 @@
+TOKEN = '5800700742:AAGxKTs70KDqVDUfLMJ4iPTrs-Rd4qOud7A'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB'
+}
